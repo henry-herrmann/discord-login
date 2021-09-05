609f8ff0f489a807d131b140df22d8a14032ea22
Henry <support@herr-mann.nrw>
2021-09-05 20:17:32 +0200

    commit

--- routers/redirect.js
+++ routers/redirect.js
@@ -6,6 +6,7 @@ router.get("/", (req, res) =>{
     if(req.query == undefined || req.query == null) return;
 
     console.log(req.query.code);
+    res.status(200).send("Worked")
 })
 
 module.exports = router;
