97cfa82561a4f6e4bbe2aa51544f031ae2f24ce3
Henry <support@herr-mann.nrw>
2021-09-05 20:24:20 +0200

    commit

--- routers/redirect.js
+++ routers/redirect.js
@@ -5,7 +5,19 @@ const router = express.Router();
 router.get("/", (req, res) =>{
     if(req.query == undefined || req.query == null) return;
 
-    console.log(req.query.code);
+    const response = await axios({
+        method: "post",
+        url: "https://discord.com/api/oauth2/token",
+        data: {
+            client_id: '884131363228356669',
+            client_secret: 'FP8_yZGbSXZbiGUlkxP2ZRboN1QaBFFT',
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: "https://google.com"
+        },
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    })
+    console.log(response)
     res.status(200).send("Worked")
 })
 
