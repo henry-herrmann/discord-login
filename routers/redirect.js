edb87e37056553d5a50a1b2af4d690b8c9801c6c
Henry <support@herr-mann.nrw>
2021-09-05 20:24:36 +0200

    commit

--- routers/redirect.js
+++ routers/redirect.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios")
 const router = express.Router();
 
-router.get("/", (req, res) =>{
+router.get("/", async (req, res) =>{
     if(req.query == undefined || req.query == null) return;
 
     const response = await axios({
