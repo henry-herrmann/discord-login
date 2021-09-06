c4bd0b6ffb32a48ddbece193d26f531a915b0df8
Henry <support@herr-mann.nrw>
2021-09-06 15:53:02 +0200

    commit

--- routers/redirect.js
+++ routers/redirect.js
@@ -20,14 +20,10 @@ router.get("/", async (req, res) =>{
         }
     })
     const json = await token_response.json();
+
+    console.log(json)
     
-    const user = await fetch('https://discord.com/api/users/@me', {
-        headers: {
-            authorization: `${json.token_type} ${access_token}`
-        }
-    })
-    const user_json = await user.json();
-    console.log(user_json)
+    
     
     res.status(200).send("Worked")
 })
