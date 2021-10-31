28a13d68145762e9660e4ceb5058f18e5099be14
Henry <henryherrmann.tv@gmail.com>
2021-10-31 12:53:50 +0100

    commit

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import route from "./routers/guilds.mjs";
+import auth from "./routers/auth";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cookieParser());
+app.use("/auth/", auth);
 app.use("/guilds", route);
 
 app.listen(23456)
