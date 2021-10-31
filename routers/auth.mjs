3c2aeea046e4148cc3c0af26fbbaedccd695faf6
Henry <henryherrmann.tv@gmail.com>
2021-10-31 13:18:46 +0100

    commit

--- routers/auth.mjs
+++ routers/auth.mjs
@@ -31,6 +31,7 @@ router.get("/", async (req, res) =>{
             res.redirect("http://202.61.201.124:23456/guilds")
         }else if(Object.keys(req.cookies).length != 0){
             res.clearCookie("user")
+            console.log(req.cookies)
             const access_token = req.cookies.data.split(";")[0];
             const token_type = req.cookies.data.split(";")[1];
             const expires_in = req.cookies.data.split(";")[2];
