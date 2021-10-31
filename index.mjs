988a8567c39c74907ae0624d0f61bd9dd0b9fee1
Henry <henryherrmann.tv@gmail.com>
2021-10-31 12:54:04 +0100

    commit

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import route from "./routers/guilds.mjs";
+import guilds from "./routers/guilds.mjs";
 import auth from "./routers/auth";
 import cookieParser from "cookie-parser";
 
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use("/auth/", auth);
-app.use("/guilds", route);
+app.use("/guilds", guilds);
 
 app.listen(23456)
 console.log("[REST-API] Listening on port 23456")
